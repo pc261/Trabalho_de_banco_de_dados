0eb3887fae222c65cf959680b4ac69df8f04294f
Felipe Lira dos santos <santosfelipeliipe@gmail.com>
2023-09-05 20:10:17 -0300

    terceiro

--- trabalho.sql
+++ trabalho.sql
@@ -5,3 +5,9 @@
  select nome
  from autores 
  where nascimento <= '1900-01-01';
+
+  -- terceiro --
+select  titulo ,autor_id, nome
+from livros l 
+join autores a on a.id = l.autor_id
+where a.nome = 'J.K. Rowling';
