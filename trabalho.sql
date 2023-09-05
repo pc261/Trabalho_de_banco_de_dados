ababec24eb7d249f4934d9c030502896bc22c5fb
Felipe Lira dos santos <santosfelipeliipe@gmail.com>
2023-09-05 20:10:40 -0300

    quarto

--- trabalho.sql
+++ trabalho.sql
@@ -11,3 +11,9 @@ select  titulo ,autor_id, nome
 from livros l 
 join autores a on a.id = l.autor_id
 where a.nome = 'J.K. Rowling';
+
+-- quarto -- 
+select aluno_id, nome, curso
+from alunos a
+left join matriculas m on m.id  = a.id
+where curso = "Engenharia de Software";
